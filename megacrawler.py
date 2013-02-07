5ed01ceacb5b45cb636ba73e0c8a627d8e80960e
Light <leenf@tcd.ie>
2013-02-07 23:04:09 +0000

    Added threading - helps us when waiting for url to load

--- megacrawler.py
+++ megacrawler.py
@@ -3,19 +3,19 @@ import urllib2
 import re
 import psycopg2
 import sys
+import threading
 
 megaLinks = set()
 allLinks = set() # Adding this makes each parse slower but does less parses
 count = 0
 maxDepth = 2
+conn = psycopg2.connect("dbname=ogatest user=postgres password=1234")
 
 def crawl(start, megaLinks, allLinks, depth):
   links = set()
 
   parse(start, megaLinks, allLinks, links)
-  links = {link for link in links if link not in allLinks} # See set comprehensions when I forget what this is.
-  for link in links:
-    allLinks.add(link)
+
   if(depth>maxDepth): return
 
   global count
@@ -27,11 +27,15 @@ def crawl(start, megaLinks, allLinks, depth):
   for link in links:
     count+=1
     localcount+=1
+    t = threading.Thread(target=crawl, args=(link, megaLinks, allLinks, depth+1))
+    #t.daemon = True
+    t.start()
     print "%s %d - %d - %d/%d Crawling: %s" % (depthstring, count, len(megaLinks), localcount, linkcount, link)
-    sys.stdout.flush()
-    crawl(link, megaLinks, allLinks, depth+1)
+    #sys.stdout.flush()
+    #crawl(link, megaLinks, allLinks, depth+1)
 
 def parse(page, megaLinks, allLinks, links):
+  #Try to load page and get links
   try:
     hdr = { 'User-Agent' : 'Just a friendly bot passing through!' }
     req = urllib2.Request(page, headers=hdr)
@@ -42,32 +46,36 @@ def parse(page, megaLinks, allLinks, links):
         megalinkre = re.search('(https://)(www.|)mega.co.nz/#!.{52}$', s)
         nonmegalinkre = re.search('(http://)(www.|)', s)
         if megalinkre and s not in megaLinks:
-          megaLinks.add(s)
+          megaLinks.add(s)#dont really need to keep this
+          store(s) # make this async else it will slow it down even MORE
         elif nonmegalinkre and s not in links:
           links.add(s)
         elif s!=None and s[0]=='/' and (page+s) not in links:
           links.add(page+s)
   except urllib2.URLError, e:
     print e
-  except: 
-    print sys.exc_info()[0]
+  except IndexError, e: 
+    print e
 
-crawl("http://www.reddit.com/", megaLinks, allLinks, 0)
+  #Only add NEW links to the collection of all the links
+  links = {link for link in links if link not in allLinks} # See set comprehensions when I forget what this is.
+  for link in links:
+    allLinks.add(link)
 
-print len(megaLinks)
-print "Adding links to DB"
-
-conn = psycopg2.connect("dbname=ogatest user=postgres password=1234")
-cur = conn.cursor()
-
-for link in megaLinks:
+def store(link):
+  cur = conn.cursor()
   try:
     cur.execute("INSERT INTO tmplinks(link) VALUES(%s);", (link,))
   except psycopg2.IntegrityError:
     conn.rollback()
+  cur.close()
+
+crawl("http://www.reddit.com/r/megalinks", megaLinks, allLinks, 0)
+print "beep"
+print len(megaLinks)
+
 print "*********************************"
 print "Out of %d links, %d were megalinks." % (len(allLinks),len(megaLinks))
 
-cur.close()
 conn.commit()
 conn.close()
