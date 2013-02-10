18585a925ffb4425361cbc36a95918ec09907360
Light <leenf@tcd.ie>
2013-02-10 04:12:36 +0000

    LET THE LINKS FLOW

--- megacrawler2.py
+++ megacrawler2.py
@@ -9,10 +9,11 @@ import psycopg2
 links = ["http://www.reddit.com/r/megalinks"]
 
 link_queue = Queue.Queue()
-chunk_queue = Queue.Queue()
+chunk_queue = Queue.Queue(10)
+mega_queue = Queue.Queue()
+list_queue = Queue.Queue()
 
 class ThreadUrl(threading.Thread):
-  """boop"""
   def __init__(self, link_queue, chunk_queue):
     threading.Thread.__init__(self)
     self.link_queue = link_queue
@@ -22,17 +23,17 @@ class ThreadUrl(threading.Thread):
     while True:
       #grabs link from queue
       link = self.link_queue.get()
-
+      print self.link_queue.qsize()," - ",link #should be unique and clean
       #grabs urls of links and then grabs chunk of webpage
-      #
-
       try:
         hdr = { 'User-Agent' : 'Just a friendly bot passing through!' }
         page = urllib2.Request(link, headers=hdr)
         url = urllib2.urlopen(page)
         chunk = url.read()
       except urllib2.URLError, e:
+        print e
         self.link_queue.task_done()
+        return
 
       #place chunk into out queue
       self.chunk_queue.put(chunk)
@@ -40,25 +41,60 @@ class ThreadUrl(threading.Thread):
       #signals to queue job is done
       self.link_queue.task_done()
 
-class DatamineThread(threading.Thread):
-  def __init__(self, data_queue, newlink_queue):
+class ThreadDatamine(threading.Thread):
+  def __init__(self, data_queue, mega_queue, list_queue):
     threading.Thread.__init__(self)
     self.chunk_queue = chunk_queue
-    self.newlink_queue = newlink_queue
+    self.mega_queue = mega_queue
+    self.list_queue = list_queue
 
   def run(self):
     while True:
       #grabs link from queue
       chunk = self.chunk_queue.get()
-
       #parse the chunk
       soup = BeautifulSoup(chunk)
       for link in soup.find_all('a'):
-        self.newlink_queue.put(unicode(link.get('href')))
+        s = unicode(link.get('href'))
+        linkregex = re.search('(http://)(www.|)', s)
+        if(linkregex):
+          self.list_queue.put(s)
+        else:
+          megaregex = re.search('(https://)(www.|)mega.co.nz/#!.{52}$', s)
+          if(megaregex):
+            self.mega_queue.put(s)
       #signals to queue job is done
       self.chunk_queue.task_done()
 
+class ThreadList(threading.Thread):
+  def __init__(self, list_queue, link_queue):
+    threading.Thread.__init__(self)
+    self.list_queue = list_queue
+    self.link_queue = link_queue
+    self.allLinks = set([])
+
+  def run(self):
+    while True:
+      link = self.list_queue.get()
+      if(link not in self.allLinks):
+        self.allLinks.add(link)
+        self.link_queue.put(link)
+
+      self.list_queue.task_done()
+
+class ThreadStore(threading.Thread):
+  def __init__(self, mega_queue):
+    threading.Thread.__init__(self)
+    self.mega_queue = mega_queue
+
+  def run(self):
+    while True:
+      link = self.mega_queue.get()
+      store(link)
+      self.mega_queue.task_done()
+
 def store(link):
+  conn = psycopg2.connect("dbname=ogatest user=postgres password=1234")
   cur = conn.cursor()
   try:
     cur.execute("INSERT INTO tmplinks(link) VALUES(%s);", (link,))
@@ -66,57 +102,38 @@ def store(link):
     conn.rollback()
     return False
   cur.close()
+  conn.commit()
+  conn.close()
   return True
 
 start = time.time()
 def main():
-  max_depth = 2
-  depth = 0
-  count = 0
-  megacount = 0
-  while depth<max_depth:
-    depth += 1
-    newlink_queue = Queue.Queue()
-    #spawn a pool of threads, and pass them queue instance
-    for i in range(5):
-      t = ThreadUrl(link_queue, chunk_queue)
-      t.setDaemon(True)
-      t.start()
 
-    #populate queue with data
-    for link in links:
-      link_queue.put(link)
+  #spawn a pool of threads, and pass them queue instance
+  for i in range(10):
+    t = ThreadUrl(link_queue, chunk_queue)
+    t.start()
 
-    for i in range(5):
-      dt = DatamineThread(chunk_queue, newlink_queue)
-      dt.setDaemon(True)
-      dt.start()
+  #init population of queue with data
+  for link in links:
+    link_queue.put(link)
 
-    #wait on the queue until everything has been processed
-    link_queue.join()
-    chunk_queue.join()
-    #newlink_queue.join()
+  for i in range(100):
+    u = ThreadDatamine(chunk_queue, mega_queue, list_queue)
+    u.start()
 
-#WHAT THE FUCK DID I DO HERE
-#Give DB own thread to store shit 
-#make this flow better
-#shouldnt be so loopy like this
-    while newlink_queue.qsize():
-      link = newlink_queue.get()
-      #print link
-      megalinkre = re.search('(https://)(www.|)mega.co.nz/#!.{52}$', link)
-      nonmegalinkre = re.search('(http://)(www.|)', link)
-      if megalinkre:
-        if store(link): megacount+=1
-      elif nonmegalinkre:
-        count+=1
-        link_queue.put(link)
-      #link_queue.put(link)
-      newlink_queue.task_done()
-  print "END - %d/%d megalinks/links" % (megacount,count)
+  for i in range(1):
+    s = ThreadList(list_queue, link_queue)
+    s.start()
 
-conn = psycopg2.connect("dbname=ogatest user=postgres password=1234")
+  for i in range(1):
+    v = ThreadStore(mega_queue)
+    v.start()
+
+  #wait on the queue until everything has been processed
+  link_queue.join()
+  chunk_queue.join()
+  mega_queue.join()
+
 main()
 print "Elapsed Time: %s" % (time.time() - start)
-conn.commit()
-conn.close()
